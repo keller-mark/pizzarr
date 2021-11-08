c252f8bbbef4d5895e655481397df2399e436c27
mkeller <7525285+keller-mark@users.noreply.github.com>
2021-11-07 21:33:11 -0500

    Indentation

--- R/numcodecs.R
+++ R/numcodecs.R
@@ -7,25 +7,25 @@
 #' @rdname Codec
 #' @export
 Codec <- R6::R6Class("Codec",
-                     public = list(
-                       #' @description
-                       #' Compress data.
-                       #' @param buf The un-compressed data.
-                       #' @return Compressed data.
-                       encode = function(buf) {
-                         return(buf)
-                       },
-                       #' @description
-                       #' Decompress data.
-                       #' @param buf The compressed data.
-                       #' @return Un-compressed data.
-                       decode = function(buf) {
-                         return(buf)
-                       },
-                       get_meta = function() {
-                         return(jsonlite::unbox(NA))
-                       }
-                     )
+   public = list(
+     #' @description
+     #' Compress data.
+     #' @param buf The un-compressed data.
+     #' @return Compressed data.
+     encode = function(buf) {
+       return(buf)
+     },
+     #' @description
+     #' Decompress data.
+     #' @param buf The compressed data.
+     #' @return Un-compressed data.
+     decode = function(buf) {
+       return(buf)
+     },
+     get_meta = function() {
+       return(jsonlite::unbox(NA))
+     }
+   )
 )
 
 
@@ -38,34 +38,34 @@ Codec <- R6::R6Class("Codec",
 #' @rdname Zstd
 #' @export
 Zstd <- R6::R6Class("Zstd",
-                    inherit = Codec,
-                    public = list(
-                      #' @field level The compression level.
-                      #' @keywords internal
-                      level = NULL,
-                      #' @description
-                      #' Create a new ZSTD compressor.
-                      #' @param level The compression level, between 1 and 22.
-                      #' @return A new `Zstd` object.
-                      initialize = function(level = 1) {
-                        self$level <- level
-                      },
-                      encode = function(buf) {
-                        # Reference: https://github.com/traversc/qs/blob/84e30f4/R/RcppExports.R#L16
-                        result <- qs::zstd_compress_raw(buf, self$level)
-                        return(result)
-                      },
-                      decode = function(buf) {
+  inherit = Codec,
+  public = list(
+    #' @field level The compression level.
+    #' @keywords internal
+    level = NULL,
+    #' @description
+    #' Create a new ZSTD compressor.
+    #' @param level The compression level, between 1 and 22.
+    #' @return A new `Zstd` object.
+    initialize = function(level = 1) {
+      self$level <- level
+    },
+    encode = function(buf) {
+      # Reference: https://github.com/traversc/qs/blob/84e30f4/R/RcppExports.R#L16
+      result <- qs::zstd_compress_raw(buf, self$level)
+      return(result)
+    },
+    decode = function(buf) {
 
-                      },
-                      get_meta = function() {
-                        meta <- list(
-                          id = jsonlite::unbox("zstd"),
-                          level = jsonlite::unbox(self$level)
-                        )
-                        return(meta)
-                      }
-                    )
+    },
+    get_meta = function() {
+      meta <- list(
+        id = jsonlite::unbox("zstd"),
+        level = jsonlite::unbox(self$level)
+      )
+      return(meta)
+    }
+  )
 )
 
 #' LZ4 compressor for Zarr
@@ -77,46 +77,46 @@ Zstd <- R6::R6Class("Zstd",
 #' @rdname LZ4
 #' @export
 LZ4 <- R6::R6Class("LZ4",
-                   inherit = Codec,
-                   public = list(
-                     #' @field acceleration The compression level.
-                     #' @keywords internal
-                     acceleration = NULL,
-                     #' @description
-                     #' Create a new LZ4 compressor.
-                     #' @param acceleration The compression level.
-                     #' @return A new `LZ4` object.
-                     initialize = function(acceleration = 1) {
-                       self$acceleration <- acceleration
-                     },
-                     encode = function(buf) {
-                       # Reference: https://github.com/traversc/qs/blob/84e30f4/R/RcppExports.R#L24
-                       body <- qs::lz4_compress_raw(buf, self$acceleration)
+   inherit = Codec,
+   public = list(
+     #' @field acceleration The compression level.
+     #' @keywords internal
+     acceleration = NULL,
+     #' @description
+     #' Create a new LZ4 compressor.
+     #' @param acceleration The compression level.
+     #' @return A new `LZ4` object.
+     initialize = function(acceleration = 1) {
+       self$acceleration <- acceleration
+     },
+     encode = function(buf) {
+       # Reference: https://github.com/traversc/qs/blob/84e30f4/R/RcppExports.R#L24
+       body <- qs::lz4_compress_raw(buf, self$acceleration)
 
-                       # The compressed output includes a 4-byte header storing the original size
-                       # of the decompressed data as a little-endian 32-bit integer.
-                       # Reference: https://numcodecs.readthedocs.io/en/stable/lz4.html#numcodecs.lz4.compress
-                       orig_size <- length(buf)
-                       header <- as.raw(c(
-                         bitwAnd(orig_size, 0xff),
-                         bitwAnd(bitwShiftR(orig_size, 8), 0xff),
-                         bitwAnd(bitwShiftR(orig_size, 16), 0xff),
-                         bitwAnd(bitwShiftR(orig_size, 24), 0xff)
-                       ))
+       # The compressed output includes a 4-byte header storing the original size
+       # of the decompressed data as a little-endian 32-bit integer.
+       # Reference: https://numcodecs.readthedocs.io/en/stable/lz4.html#numcodecs.lz4.compress
+       orig_size <- length(buf)
+       header <- as.raw(c(
+         bitwAnd(orig_size, 0xff),
+         bitwAnd(bitwShiftR(orig_size, 8), 0xff),
+         bitwAnd(bitwShiftR(orig_size, 16), 0xff),
+         bitwAnd(bitwShiftR(orig_size, 24), 0xff)
+       ))
 
-                       result <- c(header, body)
+       result <- c(header, body)
 
-                       return(result)
-                     },
-                     decode = function(buf) {
+       return(result)
+     },
+     decode = function(buf) {
 
-                     },
-                     get_meta = function() {
-                       meta <- list(
-                         id = jsonlite::unbox("lz4"),
-                         acceleration = jsonlite::unbox(self$acceleration)
-                       )
-                       return(meta)
-                     }
-                   )
+     },
+     get_meta = function() {
+       meta <- list(
+         id = jsonlite::unbox("lz4"),
+         acceleration = jsonlite::unbox(self$acceleration)
+       )
+       return(meta)
+     }
+   )
 )
--- R/stores.R
+++ R/stores.R
@@ -7,29 +7,29 @@
 #' @rdname Store
 #' @export
 Store <- R6::R6Class("Store",
-                     public = list(
-                       #' @description
-                       #' Get an item from the store.
-                       #' @param key The item key.
-                       #' @return The item data in a vector of type raw.
-                       get_item = function(key) {
+   public = list(
+     #' @description
+     #' Get an item from the store.
+     #' @param key The item key.
+     #' @return The item data in a vector of type raw.
+     get_item = function(key) {
 
-                       },
-                       #' @description
-                       #' Set an item in the store.
-                       #' @param key The item key.
-                       #' @param value The item value as a vector of type raw.
-                       set_item = function(key, value) {
+     },
+     #' @description
+     #' Set an item in the store.
+     #' @param key The item key.
+     #' @param value The item value as a vector of type raw.
+     set_item = function(key, value) {
 
-                       },
-                       #' @description
-                       #' Determine whether the store contains an item.
-                       #' @param key The item key.
-                       #' @return A boolean value.
-                       contains_item = function(key) {
+     },
+     #' @description
+     #' Determine whether the store contains an item.
+     #' @param key The item key.
+     #' @return A boolean value.
+     contains_item = function(key) {
 
-                       }
-                     )
+     }
+   )
 )
 
 
@@ -43,53 +43,53 @@ Store <- R6::R6Class("Store",
 #' @rdname DirectoryStore
 #' @export
 DirectoryStore <- R6::R6Class("DirectoryStore",
-                              inherit = Store,
-                              public = list(
-                                #' @field root The path to the root of the store.
-                                #' @keywords internal
-                                root = NULL,
-                                #' @description
-                                #' Create a new file system store.
-                                #' @param root The path to the root of the store.
-                                #' @return A new `DirectoryStore` object.
-                                initialize = function(root) {
-                                  self$root <- root
-                                  if(!dir.exists(root)) {
-                                    dir.create(root, recursive = TRUE, showWarnings = FALSE)
-                                  }
-                                },
-                                #' @description
-                                #' Get an item from the store.
-                                #' @param key The item key.
-                                #' @return The item data in a vector of type raw.
-                                get_item = function(key) {
-                                  fp <- file.path(self$root, key)
-                                  fp_size <- file.info(fp)$size
-                                  fp_pointer <- file(fp, "rb")
-                                  fp_data <- readBin(fp_pointer, what = "raw", n = fp_size)
-                                  close(fp_pointer)
-                                  return(fp_data)
-                                },
-                                #' @description
-                                #' Set an item in the store.
-                                #' @param key The item key.
-                                #' @param value The item value as a vector of type raw.
-                                set_item = function(key, value) {
-                                  fp <- file.path(self$root, key)
-                                  dir.create(dirname(fp), recursive = TRUE, showWarnings = FALSE)
-                                  fp_pointer <- file(fp, "wb")
-                                  writeBin(value, fp_pointer)
-                                  close(fp_pointer)
-                                },
-                                #' @description
-                                #' Determine whether the store contains an item.
-                                #' @param key The item key.
-                                #' @return A boolean value.
-                                contains_item = function(key) {
-                                  fp <- file.path(self$root, key)
-                                  return(file.exists(fp))
-                                }
-                              )
+  inherit = Store,
+  public = list(
+    #' @field root The path to the root of the store.
+    #' @keywords internal
+    root = NULL,
+    #' @description
+    #' Create a new file system store.
+    #' @param root The path to the root of the store.
+    #' @return A new `DirectoryStore` object.
+    initialize = function(root) {
+      self$root <- root
+      if(!dir.exists(root)) {
+        dir.create(root, recursive = TRUE, showWarnings = FALSE)
+      }
+    },
+    #' @description
+    #' Get an item from the store.
+    #' @param key The item key.
+    #' @return The item data in a vector of type raw.
+    get_item = function(key) {
+      fp <- file.path(self$root, key)
+      fp_size <- file.info(fp)$size
+      fp_pointer <- file(fp, "rb")
+      fp_data <- readBin(fp_pointer, what = "raw", n = fp_size)
+      close(fp_pointer)
+      return(fp_data)
+    },
+    #' @description
+    #' Set an item in the store.
+    #' @param key The item key.
+    #' @param value The item value as a vector of type raw.
+    set_item = function(key, value) {
+      fp <- file.path(self$root, key)
+      dir.create(dirname(fp), recursive = TRUE, showWarnings = FALSE)
+      fp_pointer <- file(fp, "wb")
+      writeBin(value, fp_pointer)
+      close(fp_pointer)
+    },
+    #' @description
+    #' Determine whether the store contains an item.
+    #' @param key The item key.
+    #' @return A boolean value.
+    contains_item = function(key) {
+      fp <- file.path(self$root, key)
+      return(file.exists(fp))
+    }
+  )
 )
 
 
@@ -104,94 +104,94 @@ DirectoryStore <- R6::R6Class("DirectoryStore",
 #' @rdname MemoryStore
 #' @export
 MemoryStore <- R6::R6Class("MemoryStore",
-                           inherit = Store,
-                           public = list(
-                             #' @field root The root list for the store.
-                             #' @keywords internal
-                             root = NULL,
-                             #' @description
-                             #' Create a new memory store.
-                             #' @return A new `MemoryStore` object.
-                             initialize = function() {
-                               self$root <- obj_list()
-                             },
-                             #' @description
-                             #' Get the parent of an item.
-                             #' @keywords internal
-                             #' @param item The item key.
-                             #' @return A list with the keys `parent` and `key`.
-                             get_parent = function(item) {
-                               parent <- self$root
-                               segments <- strsplit(item, "/")[[1]]
-                               for(k in segments[1:length(segments)-1]) {
-                                 parent <- parent[[k]]
-                                 if(!is.list(parent)) {
-                                   stop("KeyError:", item)
-                                 }
-                               }
-                               return(list(parent = parent, key = segments[length(segments)]))
-                             },
-                             #' @description
-                             #' Get an item from the store.
-                             #' @param item The item key.
-                             #' @return The item data in a vector of type raw.
-                             get_item = function(item) {
-                               parent_and_key <- self$get_parent(item)
-                               parent <- parent_and_key$parent
-                               key <- parent_and_key$key
+   inherit = Store,
+   public = list(
+     #' @field root The root list for the store.
+     #' @keywords internal
+     root = NULL,
+     #' @description
+     #' Create a new memory store.
+     #' @return A new `MemoryStore` object.
+     initialize = function() {
+       self$root <- obj_list()
+     },
+     #' @description
+     #' Get the parent of an item.
+     #' @keywords internal
+     #' @param item The item key.
+     #' @return A list with the keys `parent` and `key`.
+     get_parent = function(item) {
+       parent <- self$root
+       segments <- strsplit(item, "/")[[1]]
+       for(k in segments[1:length(segments)-1]) {
+         parent <- parent[[k]]
+         if(!is.list(parent)) {
+           stop("KeyError:", item)
+         }
+       }
+       return(list(parent = parent, key = segments[length(segments)]))
+     },
+     #' @description
+     #' Get an item from the store.
+     #' @param item The item key.
+     #' @return The item data in a vector of type raw.
+     get_item = function(item) {
+       parent_and_key <- self$get_parent(item)
+       parent <- parent_and_key$parent
+       key <- parent_and_key$key
 
-                               if(key %in% names(parent)) {
-                                 value <- parent[[key]]
-                               } else {
-                                 stop("KeyError:", item)
-                               }
-                               return(value)
-                             },
-                             #' @description
-                             #' Set an item in the store.
-                             #' @param item The item key.
-                             #' @param value The item value as a vector of type raw.
-                             set_item = function(item, value) {
-                               segments <- strsplit(item, "/")[[1]]
-                               if(length(segments) > 1) {
-                                 for(i in 1:(length(segments)-1)) {
-                                   k <- segments[i]
-                                   if(i == 1 && k %in% names(self$root)) {
-                                     if(!is.list(self$root[[k]])) {
-                                       stop("KeyError:", item)
-                                     }
-                                   } else if(i > 1 && k %in% names(self$root[[segments[1:(i-1)]]])) {
-                                     if(!is.list(self$root[[segments[1:i]]])) {
-                                       stop("KeyError:", item)
-                                     }
-                                   } else {
-                                     self$root[[segments[1:i]]] <- obj_list()
-                                   }
-                                 }
-                               }
-                               self$root[[segments]] <- value
-                             },
-                             #' @description
-                             #' Determine whether the store contains an item.
-                             #' @param item The item key.
-                             #' @return A boolean value.
-                             contains_item = function(item) {
-                               result <- tryCatch({
-                                 parent_and_key <- self$get_parent(item)
-                                 parent <- parent_and_key$parent
-                                 key <- parent_and_key$key
-                                 if(key %in% names(parent)) {
-                                   value <- parent[[key]]
-                                   return(!is.list(value))
-                                 } else {
-                                   return(FALSE)
-                                 }
-                               }, error = function(e) {
-                                 return(FALSE)
-                               })
-                               return(result)
-                             }
-                           )
+       if(key %in% names(parent)) {
+         value <- parent[[key]]
+       } else {
+         stop("KeyError:", item)
+       }
+       return(value)
+     },
+     #' @description
+     #' Set an item in the store.
+     #' @param item The item key.
+     #' @param value The item value as a vector of type raw.
+     set_item = function(item, value) {
+       segments <- strsplit(item, "/")[[1]]
+       if(length(segments) > 1) {
+         for(i in 1:(length(segments)-1)) {
+           k <- segments[i]
+           if(i == 1 && k %in% names(self$root)) {
+             if(!is.list(self$root[[k]])) {
+               stop("KeyError:", item)
+             }
+           } else if(i > 1 && k %in% names(self$root[[segments[1:(i-1)]]])) {
+             if(!is.list(self$root[[segments[1:i]]])) {
+               stop("KeyError:", item)
+             }
+           } else {
+             self$root[[segments[1:i]]] <- obj_list()
+           }
+         }
+       }
+       self$root[[segments]] <- value
+     },
+     #' @description
+     #' Determine whether the store contains an item.
+     #' @param item The item key.
+     #' @return A boolean value.
+     contains_item = function(item) {
+       result <- tryCatch({
+         parent_and_key <- self$get_parent(item)
+         parent <- parent_and_key$parent
+         key <- parent_and_key$key
+         if(key %in% names(parent)) {
+           value <- parent[[key]]
+           return(!is.list(value))
+         } else {
+           return(FALSE)
+         }
+       }, error = function(e) {
+         return(FALSE)
+       })
+       return(result)
+     }
+   )
 )
 
 
