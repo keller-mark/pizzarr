d84f6a62ea016a4684f6d02c1ac60de4084a8626
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-05-22 16:40:03 -0400

    Update

---
+++ R/constants.R
@@ -0,0 +1,4 @@
+# v2 store keys
+ARRAY_META_KEY <- '.zarray'
+GROUP_META_KEY <- '.zgroup'
+ATTRS_KEY <- '.zattrs'
--- R/utils.R
+++ R/utils.R
@@ -54,9 +54,21 @@ matrix_to_zarr <- function(matrix, rows, cols, store, compressor = NA) {
 #'
 #' A helper function to construct an empty list which converts to a JSON object rather than a JSON array.
 #'
+#' @param ... A variable number of list entries.
 #' @return An empty named list.
+#' 
 #' @keywords internal
-obj_list <- function() {
-  retval <- setNames(list(), character(0))
+#' @export
+#' @examples
+#' default_window <- obj_list(
+#'   min = 0,
+#'   max = 255
+#' )
+obj_list <- function(...) {
+  retval <- stats::setNames(list(), character(0))
+  param_list <- list(...)
+  for(key in names(param_list)) {
+    retval[[key]] = param_list[[key]]
+  }
   retval
 }
