35b02ff06a031188e0ad81525119fd9473782d0b
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2024-02-14 11:07:07 -0500

    Convenience functions for save_array and open_array (#63)

--- tests/testthat/test-creation.R
+++ tests/testthat/test-creation.R
@@ -24,3 +24,19 @@ test_that("zarr_create zero-dimensional", {
     z <- zarr_create(shape=dim(a), dtype="<f4", fill_value=NA)
     expect_equal(z$get_shape(), NULL)
 })
+
+test_that("zarr_save_array and zarr_open_array", {
+    a <- array(data=1:20, dim=c(2, 10))
+    z <- zarr_create_array(data=a, shape=dim(a), dtype="<f4", fill_value=NA)
+    zarr_save_array(file.path("test_data", "test_zarr_save_array.zarr"), z, overwrite = TRUE)
+
+    z2 <- zarr_open_array(file.path("test_data", "test_zarr_save_array.zarr"))
+    selection <- z2$get_item("...")
+
+    expect_equal(z$get_shape(), c(2, 10))
+    expect_equal(z2$get_shape(), c(2, 10))
+
+    expect_equal(a, selection$data)
+
+    unlink(file.path("test_data", "test_zarr_save_array.zarr"), recursive = TRUE)
+})
