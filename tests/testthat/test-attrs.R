0c58e8b47bb42c3a9d298dd611bbb74e7c0c50fa
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2024-05-07 11:23:28 -0400

    Support modification of attributes

--- tests/testthat/test-attrs.R
+++ tests/testthat/test-attrs.R
@@ -6,3 +6,96 @@ test_that("Can get attributes for empty store (without throwing a KeyError)", {
     
     expect_equal(g$get_attrs()$to_list(), obj_list())
 })
+
+test_that("Can set attributes", {
+    store <- MemoryStore$new()
+    g <- zarr_open_group(store)
+    attrs <- g$get_attrs()
+
+    attrs$set_item("foo", "bar")
+    expect_equal(attrs$to_list(), obj_list(foo = "bar"))
+
+    attrs$set_item("baz", "qux")
+    expect_equal(attrs$to_list(), obj_list(foo = "bar", baz = "qux"))
+})
+
+test_that("Can update attributes", {
+    store <- MemoryStore$new()
+    g <- zarr_open_group(store)
+    attrs <- g$get_attrs()
+
+    attrs$set_item("foo", "bar")
+    expect_equal(attrs$to_list(), obj_list(foo = "bar"))
+
+    attrs$set_item("baz", "qux")
+    expect_equal(attrs$to_list(), obj_list(foo = "bar", baz = "qux"))
+
+    attrs$set_item("foo", "baz")
+    expect_equal(attrs$to_list(), obj_list(foo = "baz", baz = "qux"))
+})
+
+test_that("Can delete attributes", {
+    store <- MemoryStore$new()
+    g <- zarr_open_group(store)
+    attrs <- g$get_attrs()
+
+    attrs$set_item("foo", "bar")
+    expect_equal(attrs$to_list(), obj_list(foo = "bar"))
+
+    attrs$set_item("baz", "qux")
+    expect_equal(attrs$to_list(), obj_list(foo = "bar", baz = "qux"))
+
+    attrs$del_item("foo")
+    expect_equal(attrs$to_list(), obj_list(baz = "qux"))
+    
+    attrs$del_item("baz")
+    expect_equal(attrs$to_list(), obj_list())
+})
+
+test_that("Can set attributes with cache FALSE", {
+    store <- MemoryStore$new()
+    g <- zarr_open_group(store)
+    attrs <- g$get_attrs()
+    attrs$cache <- FALSE
+
+    attrs$set_item("foo", "bar")
+    expect_equal(attrs$to_list(), obj_list(foo = "bar"))
+
+    attrs$set_item("baz", "qux")
+    expect_equal(attrs$to_list(), obj_list(foo = "bar", baz = "qux"))
+})
+
+test_that("Can update attributes with cache FALSE", {
+    store <- MemoryStore$new()
+    g <- zarr_open_group(store)
+    attrs <- g$get_attrs()
+    attrs$cache <- FALSE
+
+    attrs$set_item("foo", "bar")
+    expect_equal(attrs$to_list(), obj_list(foo = "bar"))
+
+    attrs$set_item("baz", "qux")
+    expect_equal(attrs$to_list(), obj_list(foo = "bar", baz = "qux"))
+
+    attrs$set_item("foo", "baz")
+    expect_equal(attrs$to_list(), obj_list(foo = "baz", baz = "qux"))
+})
+
+test_that("Can delete attributes with cache FALSE", {
+    store <- MemoryStore$new()
+    g <- zarr_open_group(store)
+    attrs <- g$get_attrs()
+    attrs$cache <- FALSE
+
+    attrs$set_item("foo", "bar")
+    expect_equal(attrs$to_list(), obj_list(foo = "bar"))
+
+    attrs$set_item("baz", "qux")
+    expect_equal(attrs$to_list(), obj_list(foo = "bar", baz = "qux"))
+
+    attrs$del_item("foo")
+    expect_equal(attrs$to_list(), obj_list(baz = "qux"))
+    
+    attrs$del_item("baz")
+    expect_equal(attrs$to_list(), obj_list())
+})
