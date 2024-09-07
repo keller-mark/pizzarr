1aa13ab0756a2b3cb26cfb9877dff5832b75f176
Artur-man <artur-man@hotmail.com>
2024-09-07 22:38:33 +0200

    remove functions and documentation of int() utilities for now #108

--- R/int.R
+++ R/int.R
@@ -35,38 +35,38 @@ Int <- R6::R6Class("Int",
                      )
 )
 
-#' Convenience function for the internal Int class constructor.
-#' @param index The integer index.
-#' @param zero_based The index of the dimension. By default, FALSE for R-like behavior.
-#' @return A Int instance with the specified parameters.
-#' @export
-int <- function(index, zero_based = FALSE) {
-  index_offset <- ifelse(zero_based, 0, -1)
-  if(!is_na(index) && is.numeric(index)) {
-    index <- index + index_offset
-  }
-  # Assumed to be zero-based
-  # and stop-inclusive
-  return(Int$new(
-    index = index
-  ))
-}
-
-#' Convenience function for the internal Int class constructor 
-#' with zero-based indexing 
-#' @param index integer index
-#' @export
-zb_int <- function(index) {
-  return(int(index, zero_based = TRUE))
-}
-
-#' Check if a value is a Int instance.
-#' @param s The value to check.
-#' @return TRUE if the value is a Slice instance, FALSE otherwise.
-#' @export
-is_int<- function(s) {
-  if(class(s)[[1]] == "Int") {
-    return(TRUE)
-  }
-  return(FALSE)
-}
+#' #' Convenience function for the internal Int class constructor.
+#' #' @param index The integer index.
+#' #' @param zero_based The index of the dimension. By default, FALSE for R-like behavior.
+#' #' @return A Int instance with the specified parameters.
+#' #' @export
+#' int <- function(index, zero_based = FALSE) {
+#'   index_offset <- ifelse(zero_based, 0, -1)
+#'   if(!is_na(index) && is.numeric(index)) {
+#'     index <- index + index_offset
+#'   }
+#'   # Assumed to be zero-based
+#'   # and stop-inclusive
+#'   return(Int$new(
+#'     index = index
+#'   ))
+#' }
+#' 
+#' #' Convenience function for the internal Int class constructor 
+#' #' with zero-based indexing 
+#' #' @param index integer index
+#' #' @export
+#' zb_int <- function(index) {
+#'   return(int(index, zero_based = TRUE))
+#' }
+#' 
+#' #' Check if a value is a Int instance.
+#' #' @param s The value to check.
+#' #' @return TRUE if the value is a Slice instance, FALSE otherwise.
+#' #' @export
+#' is_int<- function(s) {
+#'   if(class(s)[[1]] == "Int") {
+#'     return(TRUE)
+#'   }
+#'   return(FALSE)
+#' }
